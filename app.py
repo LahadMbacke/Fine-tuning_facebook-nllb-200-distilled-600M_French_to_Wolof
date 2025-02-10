4eca6c0f97c9d83a82d2e92ecf99dddbffefdbc3
Lahad Mbacke <lahadmbacke00@gmail.com>
2025-02-10 20:14:30 +0100

    Recup code lang

--- app.py
+++ app.py
@@ -13,3 +13,13 @@ model = AutoModelForSeq2SeqLM.from_pretrained("facebook/nllb-200-distilled-600M"
 
 #%%
 
+# Récupérer les codes de langue via la configuration du modèle
+
+FR_CODE = tokenizer.convert_tokens_to_ids("fr_Latn")
+WO_CODE = tokenizer.convert_tokens_to_ids("wol_Latn")
+
+# Configuration du tokenizer pour indiquer la langue source
+tokenizer.src_lang = "fr_Latn"
+
+# Forcer la langue cible lors de la génération en définissant forced_bos_token_id
+model.config.forced_bos_token_id = WO_CODE
